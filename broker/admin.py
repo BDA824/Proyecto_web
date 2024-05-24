bf06329ef20d883715d1a2cd0286ad729a5a74c4
felipeleonbotero <felipeleonbotero@hotmail.com>
2024-05-24 00:26:43 -0500

    usuariosDATABASE

--- broker/admin.py
+++ broker/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Action, Buy
+from .models import User, Action, Buy, Manager, Country
 
 admin.site.register(User)
 admin.site.register(Action)
 admin.site.register(Buy)
+admin.site.register(Manager)
+admin.site.register(Country)
 
